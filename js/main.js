f877ad37381d1952d0f59634acad9afd33e78444
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-08-03 18:16:48 -0700

    Phaser.js hooked up and running.  global.js init.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("connected");
+var game = new Phaser.Game(800, 500, Phaser.Auto, 'game', {
+  init: init,
+  preload: preload,
+  create: create,
+  update: update
+});
+
+function init() {
+}
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
