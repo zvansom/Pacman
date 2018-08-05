666c413c3f6c31b370d738f0d11b974f787f7047
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-08-04 19:29:05 -0700

    sprite assets added

--- js/main.js
+++ js/main.js
@@ -9,9 +9,15 @@ function init() {
 }
 
 function preload() {
+  game.load.spritesheet('test', '../assets/spritesheet.png', 16, 16);
+
 }
 
 function create() {
+  pacman = game.add.sprite(100, 100, 'test');
+  sprite.frame = 0;
+  sprite.scale.x = 0.5;
+  sprite.scale.y = 0.5;
 }
 
 function update() {
