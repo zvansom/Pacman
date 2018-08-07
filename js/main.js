720d480f6caa0a23684f66ea15e1f024991f4623
Zach VanSomeren <zvansomeren@gmail.com>
2018-08-07 15:47:56 -0700

    Refactored code to object orient.

--- js/main.js
+++ js/main.js
@@ -1,57 +1,89 @@
-var game = new Phaser.Game(450, 500, Phaser.Auto, 'game', {
-  init: init,
-  preload: preload,
-  create: create,
-  update: update
-});
+var game = new Phaser.Game(450, 500, Phaser.Auto, 'game');
 
-function init() {
-}
+// Create a new instance of a Phaser Game and give it these additional properties.
+var PacmanGame = function(game) {
+  //  Used in storing game assets.
+  this.map = null;
+  this.layer = null;
+  this.pacman = null;
 
-function preload() {
-  // Enable physics
-  game.physics.startSystem(Phaser.Physics.ARCADE);
+  // Sets character speed
+  this.speed = 100;
 
-  // Tilemap object and image for tilemap.
-  game.load.tilemap('map', './assets/pacman_tile_map.json', null, Phaser.Tilemap.TILED_JSON);
-  game.load.image('spritesheet', './assets/spritesheet.png');
+  // The direction the character is currently traveling in.
+  this.current = Phaser.UP;
+};
 
-  // Load spritesheet
-  game.load.spritesheet('sprites', './assets/spritesheet.png', 16, 16);
+// Add methods to the game prototype.
+PacmanGame.prototype = {
+  // Initialize Phaser Arcade physics
+  init: function() {
+    this.physics.startSystem(Phaser.Physics.ARCADE);
+  },
 
-  // pacman
-  game.load.spritesheet
-}
+  // Load assets for use in game.
+  preload: function() {
+    // Tilemap object and image for tilemap.
+    this.load.tilemap('map', './assets/pacman_tile_map.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.image('spritesheet', './assets/spritesheet.png');
 
-function create() {
+    // Load spritesheet
+    this.load.spritesheet('sprites', './assets/spritesheet.png', 16, 16);
+  },
 
-  game.stage.backgroundColor = '#000000';
+  // Add assets previously loaded into the game window.
+  create: function() {
+    // Add the tile map and background image.
+    this.map = this.add.tilemap('map');
+    this.map.addTilesetImage('Pacman Assets', 'spritesheet');
+    this.layer = this.map.createLayer('Tile Layer 1');
+    this.map.setCollisionByExclusion('0', true, this.layer);
 
-  map = game.add.tilemap('map');
-  map.addTilesetImage('Pacman Assets', 'spritesheet');
+    // Add the player controlled character
+    this.pacman = this.add.sprite(32, 16, 'sprites');
+    this.pacman.frame = 3;
+    this.physics.arcade.enable(this.pacman);
 
-  layer = map.createLayer('Tile Layer 1');
+    // Add keyboard listeners.
+    this.cursors = this.input.keyboard.createCursorKeys();
 
-  pacman = game.add.sprite(32, 16, 'sprites');
-  pacman.frame = 3;
-  game.physics.enable(pacman);
+    this.move(Phaser.RIGHT);
+  },
 
-  cursors = game.input.keyboard.createCursorKeys();
+  // Handle keypress event
+  handleKeyPress: function() {
+    if (this.cursors.left.isDown) {
+      this.current = Phaser.LEFT;
+    }else if (this.cursors.right.isDown) {
+      this.current = Phaser.RIGHT;
+    }
 
-}
+    if (this.cursors.up.isDown) {
+      this.current = Phaser.UP;
+    }else if (this.cursors.down.isDown) {
+      this.current = Phaser.DOWN;
+    }
+  },
 
-function update() {
-  pacman.body.velocity.set(0);
+  move: function(direction) {
+    var speed = this.speed;
+    if (direction === Phaser.LEFT || direction === Phaser.UP) {
+      speed = -speed;
+    }
+    if (direction === Phaser.LEFT || direction === Phaser.RIGHT) {
+      this.pacman.body.velocity.x = speed;
+    } else {
+      this.pacman.body.velocity.y = speed;
+    }
+    this.current = direction;
+  },
 
-  if(cursors.left.isDown) {
-    pacman.body.velocity.x = -50;
-  } else if(cursors.right.isDown) {
-    pacman.body.velocity.x = 50;
+  update: function() {
+    this.physics.arcade.collide(this.pacman, this.layer);
+
+    this.handleKeyPress();
+    this.move(this.current);
   }
-
-  if(cursors.up.isDown) {
-    pacman.body.velocity.y = -50;
-  } else if(cursors.down.isDown) {
-    pacman.body.velocity.y = 50;
-  }
 }
+
+game.state.add('Game', PacmanGame, true);
