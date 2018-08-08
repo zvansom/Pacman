2fbc5cefd4d56b7c67e213a85534e9aa345c8b91
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-08-08 12:49:23 -0700

    Dots added.  Score increments.  Win condition test in place.

--- js/main.js
+++ js/main.js
@@ -6,15 +6,16 @@ var PacmanGame = function(game) {
   this.map = null;
   this.layer = null;
   this.pacman = null;
+  this.dots = null;
 
   this.prevX = null;
   this.prevY = null;
 
   // Sets character speed
-  this.speed = 100;
+  this.speed = 200;
 
   // The direction the character is currently traveling in.
-  this.current = Phaser.UP;
+  this.current = null;
 };
 
 // Add methods to the game prototype.
@@ -30,28 +31,52 @@ PacmanGame.prototype = {
     this.load.tilemap('map', './assets/pacman_tile_map.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('spritesheet', './assets/spritesheet.png');
 
-    // Load spritesheet
-    this.load.spritesheet('sprites', './assets/spritesheet.png', 16, 16);
+    // Load pacman
+    this.load.spritesheet('pacman', './assets/spritesheet.png', 16, 16);
+
+    // Load dot
+    this.load.spritesheet('dot', './assets/spritesheet.png', 16, 16);
+
   },
 
   // Add assets previously loaded into the game window.
   create: function() {
     // Add the tile map and background image.
     this.map = this.add.tilemap('map');
+    // Add the tileset for the maze.
     this.map.addTilesetImage('Pacman Assets', 'spritesheet');
     this.layer = this.map.createLayer('Tile Layer 1');
-    this.map.setCollisionByExclusion('0', true, this.layer);
+    // Add collision to all tiles with 'index' !== 0
+    this.map.setCollisionByExclusion('224', true, this.layer);
+
+    // Add dot tiles to map, enable physics and body.
+    this.dots = this.add.group();
+    this.dots.enableBody = true;
+
+    // Add dot to all tiles with 'index' === 0;
+    var dotCoords = this.getDotCoords();
+    for(var i = 0; i < dotCoords.length; i++) {
+      this.dots.create(dotCoords[i][0], dotCoords[i][1], 'dot', 149);
+    }
+
+    this.dots.remaining = dotCoords.length;
 
     // Add the player controlled character
-    this.pacman = this.add.sprite(32, 16, 'sprites');
-    this.pacman.frame = 3;
+    this.pacman = this.add.sprite(224, 272, 'pacman');
     this.physics.arcade.enable(this.pacman);
+    // this.pacman.body.friction = 0;
+    // this.pacman.body.drag = 0;
+    this.pacman.score = 0;
 
+    // ----- ANIMATIONS -----
     // Add the player animations
     this.pacman.animations.add('right', [0, 1, 2, 1], 10, true);
     this.pacman.animations.add('left', [14, 15, 2, 15], 10, true);
     this.pacman.animations.add('up', [28, 29, 2, 29], 10, true);
     this.pacman.animations.add('down', [42, 43, 2, 43], 10, true);
+
+    // Add dot animations
+    this.dots.callAll('animations.add', 'animations', 'flashing', [149, 152], 10, true);
 
     // Add keyboard listeners.
     this.cursors = this.input.keyboard.createCursorKeys();
@@ -59,7 +84,7 @@ PacmanGame.prototype = {
     // Spacebar included for convenience in testing.
     this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-    this.move(Phaser.RIGHT);
+    // this.move(Phaser.RIGHT);
   },
 
   // Handle keypress event
@@ -77,36 +102,41 @@ PacmanGame.prototype = {
     }
 
     if (this.spaceKey.isDown) {
-      console.log('X:', this.pacman.x);
-      console.log('Y:', this.pacman.y);
+      console.log('Dots left:', this.dots.remaining);
     }
   },
 
-  getDirection: function() {
-    // console.log('pacman x', this.pacman.x);
-    // console.log('pacman y', this.pacman.y);
-    // Get previous position
-    var prevX = this.prevX;
-    var prevY = this.prevY;
-    // console.log('prevX:', prevX);
-    // console.log('prevY:', prevY);
-    // // Get current position
+  getDotCoords: function() {
+    var tileCoords = [];
+    for(var i = 0; i < this.layer.layer.data.length; i++){
+      this.layer.layer.data[i].forEach(cell => {
+        if(cell.index === -1) {
+          tileCoords.push([cell.worldX, cell.worldY]);
+        }
+      });
+    }
+    return tileCoords;
+  },
+
+  // TODO: Need to separate into separate 'Get Direction' and 'Animate' methods.
+  // TODO: Need to catch 'twitching' bug.  (I think it's a decimal rounding error.)
+          // Only occurs when moving in y with x movement queued.
+  animatePacman: function() {
+    var prevX = Math.floor(this.prevX);
+    var prevY = Math.floor(this.prevY);
     var currX = Math.floor(this.pacman.x);
     var currY = Math.floor(this.pacman.y);
-    // console.log('currX:' + currX);
-    // console.log('currY:' + currY);
-    // // if previous position has change in x: moving left/right
+
     if(prevX > currX) {
-      console.log('left');
       this.pacman.animations.play('left');
-    } else if(prevX < currX) {
-      console.log('right');
-      this.pacman.animations.play('right');
     } else if(prevY > currY) {
       this.pacman.animations.play('up');
+    } else if(prevX < currX) {
+      this.pacman.animations.play('right');
     } else if(prevY < currY){
       this.pacman.animations.play('down');
     } else {
+      // Do nothing.
     }
     this.prevX = currX;
     this.prevY = currY;
@@ -123,14 +153,35 @@ PacmanGame.prototype = {
       this.pacman.body.velocity.y = speed;
     }
     this.current = direction;
+    this.animatePacman();
+  },
+
+  collectDot: function(pacman, dot) {
+    pacman.score += 10;
+    dot.parent.remaining--;
+    dot.kill();
   },
 
   update: function() {
     this.physics.arcade.collide(this.pacman, this.layer);
+    this.physics.arcade.overlap(this.pacman, this.dots, this.collectDot)
 
+    this.dots.callAll('play', null, 'flashing');
     this.handleKeyPress();
     this.move(this.current);
-    this.getDirection();
+
+    if(this.pacman.body.x < 0) {
+      this.pacman.reset(450, 224);
+      this.pacman.body.velocity.x = -this.speed;
+    }
+    if(this.pacman.body.x > 450) {
+      this.pacman.reset(0, 224);
+      this.pacman.body.velocity.x = this.speed;
+    }
+
+    if(this.dots.remaining === 0) {
+      console.log('Game over');
+    }
   }
 }
 
