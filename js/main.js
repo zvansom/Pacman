37dcd20f2265712939dad3aae6cd5d5674f65dfe
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-08-07 18:17:13 -0700

    Pacman animation complete.

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ var PacmanGame = function(game) {
   this.layer = null;
   this.pacman = null;
 
+  this.prevX = null;
+  this.prevY = null;
+
   // Sets character speed
   this.speed = 100;
 
@@ -44,8 +47,17 @@ PacmanGame.prototype = {
     this.pacman.frame = 3;
     this.physics.arcade.enable(this.pacman);
 
+    // Add the player animations
+    this.pacman.animations.add('right', [0, 1, 2, 1], 10, true);
+    this.pacman.animations.add('left', [14, 15, 2, 15], 10, true);
+    this.pacman.animations.add('up', [28, 29, 2, 29], 10, true);
+    this.pacman.animations.add('down', [42, 43, 2, 43], 10, true);
+
     // Add keyboard listeners.
     this.cursors = this.input.keyboard.createCursorKeys();
+
+    // Spacebar included for convenience in testing.
+    this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     this.move(Phaser.RIGHT);
   },
@@ -63,6 +75,41 @@ PacmanGame.prototype = {
     }else if (this.cursors.down.isDown) {
       this.current = Phaser.DOWN;
     }
+
+    if (this.spaceKey.isDown) {
+      console.log('X:', this.pacman.x);
+      console.log('Y:', this.pacman.y);
+    }
+  },
+
+  getDirection: function() {
+    // console.log('pacman x', this.pacman.x);
+    // console.log('pacman y', this.pacman.y);
+    // Get previous position
+    var prevX = this.prevX;
+    var prevY = this.prevY;
+    // console.log('prevX:', prevX);
+    // console.log('prevY:', prevY);
+    // // Get current position
+    var currX = Math.floor(this.pacman.x);
+    var currY = Math.floor(this.pacman.y);
+    // console.log('currX:' + currX);
+    // console.log('currY:' + currY);
+    // // if previous position has change in x: moving left/right
+    if(prevX > currX) {
+      console.log('left');
+      this.pacman.animations.play('left');
+    } else if(prevX < currX) {
+      console.log('right');
+      this.pacman.animations.play('right');
+    } else if(prevY > currY) {
+      this.pacman.animations.play('up');
+    } else if(prevY < currY){
+      this.pacman.animations.play('down');
+    } else {
+    }
+    this.prevX = currX;
+    this.prevY = currY;
   },
 
   move: function(direction) {
@@ -83,6 +130,7 @@ PacmanGame.prototype = {
 
     this.handleKeyPress();
     this.move(this.current);
+    this.getDirection();
   }
 }
 
