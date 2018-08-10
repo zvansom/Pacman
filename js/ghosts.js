4a8397765809399e5d26037ce44bde689c4ee55c
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-08-09 18:39:07 -0700

    Ghost added.  Random movement.  Queue movement function called too frequently.  Need to delay.

---
+++ js/ghosts.js
@@ -0,0 +1,30 @@
+function queueGhostMovement(ghost) {
+  if(ghost.key === 'pacman') {
+    return;
+  } else {
+    var direction = ghost.currentDirection;
+
+    switch(direction) {
+      case 'RIGHT':
+      case 'LEFT':
+        if (Math.random() > 0.5) {
+          ghost.queuedDirection = 'UP';
+        } else {
+          ghost.queuedDirection = 'DOWN';
+        }
+        break;
+
+      case 'DOWN':
+      case 'UP':
+      if (Math.random() > 0.5) {
+        ghost.queuedDirection = 'LEFT';
+      } else {
+        ghost.queuedDirection = 'RIGHT';
+      }
+      break;
+
+      default:
+        break;
+    }
+  }
+}
