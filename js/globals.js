e6346956deb62a062262eca1bdfcf87334c95b77
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-08-10 13:33:57 -0700

    Create release timer and declared release function.

--- js/globals.js
+++ js/globals.js
@@ -1,10 +1,12 @@
 var pacman, blinky, pinky, inky, clyde, dots, power_pills;
-var ghosts = [blinky, pinky, inky, clyde];
 var score = 0;
+var scoreText;
 var lives = 3;
+var livesDisplay;
 var cursors, spaceKey;
+var ghostsInPlay = 0;
 
-var vulnerableTimer, restartTimer;
+var vulnerableTimer, restartTimer, releaseTimer;
 
 var SPEED = 100;
 //  Used in storing game assets.
